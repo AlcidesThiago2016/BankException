60a6a622211986363d840fcabe0c923b5c91d3bb
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-11-28 21:58:57 -0300

    Initial commit

---
+++ src/devSuperior/application/Program.java
@@ -0,0 +1,37 @@
+package devSuperior.application;
+
+import devSuperior.entities.Account;
+
+import javax.crypto.spec.PSource;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data: ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder  = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limit: ");
+        double limit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, limit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        acc.withdraw(amount);
+        System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+
+        sc.close();
+    }
+}
